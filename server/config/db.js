f5c5e94e3a2243c4da39a80623d443252e046ebc
teslasdev <Teslasdeveloper@gmail.com>
2023-07-25 01:09:44 +0100

    fixed some

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql'
 const  connectDB = () => {
   const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database : "placetoeat"
+    host: "1.2.3.4",
+    user: "uyhd9egyeqd2z",
+    password: "aD@vin33",
+    database : "dbn5fai2jcajgl"
   });
   
   conn.connect(function(err) {
